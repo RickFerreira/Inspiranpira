abf51c9b4ea3268a72a463c568999c38e7f3d95c
Richard Ferreira Salviano <salviano.richard@academico.ifpb.edu.br>
2024-03-21 15:42:03 -0300

    Update statistics.js

--- assets/js/statistics.js
+++ assets/js/statistics.js
@@ -1,10 +1,10 @@
 // scripts/statistics.js
 
 // Função para animar os números
-function animateValue(id, start, end, duration) {
+function animateValue(id, start, end, duration, qnt) {
     var range = end - start;
     var current = start;
-    var increment = end > start ? 1 : -1;
+    var increment = end > start ? qnt : -qnt;
     var stepTime = Math.abs(Math.floor(duration / range));
     var obj = document.getElementById(id);
     var timer = setInterval(function() {
@@ -17,8 +17,8 @@ function animateValue(id, start, end, duration) {
   }
   
   // Chame a função para cada número com seus valores finais
-  animateValue("teachers", 0, 2116, 1000);
-  animateValue("cities", 0, 14, 3000);
-  animateValue("lectures", 0, 17, 3000);
-  animateValue("nps", 0, 973, 2000);
+  animateValue("pessoas", 0, 27000, 5000, 24);
+  animateValue("eventos", 0, 470, 5000, 1);
+  animateValue("palestras", 0, 17, 5000, 1);
+  animateValue("cidades", 0, 14, 5000, 1);
   
