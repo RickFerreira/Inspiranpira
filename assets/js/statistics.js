71e55437ad034d5aa7121b9e02967050c11bb277
RickFerreira <richard.ferreira.ext@synchro.com.br>
2024-03-23 16:02:06 -0300

    Ajustando seções

--- assets/js/statistics.js
+++ assets/js/statistics.js
@@ -1,5 +1,35 @@
 // scripts/statistics.js
 
+// Variáveis globais para armazenar os timers das animações
+let timers = {};
+
+// Função para animar os números quando a seção de estatísticas estiver visível
+function animateStatistics() {
+    const options = {
+        threshold: 0.2 // Define o limite de visibilidade para 20%
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                // Se a seção estiver visível, inicia a animação
+                animateValue("pessoas", 0, 27000, 5000, 24);
+                animateValue("eventos", 0, 470, 5000, 1);
+                animateValue("palestras", 0, 17, 5000, 1);
+                animateValue("cidades", 0, 14, 5000, 1);
+            } else {
+                // Se a seção não estiver visível, redefine os valores para zero e limpa os timers
+                resetValues();
+            }
+        });
+    }, options);
+
+    const elementsToAnimate = document.querySelectorAll('.statistics');
+    elementsToAnimate.forEach(element => {
+        observer.observe(element);
+    });
+}
+
 // Função para animar os números
 function animateValue(id, start, end, duration, qnt) {
     var range = end - start;
@@ -7,18 +37,33 @@ function animateValue(id, start, end, duration, qnt) {
     var increment = end > start ? qnt : -qnt;
     var stepTime = Math.abs(Math.floor(duration / range));
     var obj = document.getElementById(id);
-    var timer = setInterval(function() {
-      current += increment;
-      obj.innerHTML = current;
-      if (current == end) {
-        clearInterval(timer);
-      }
+
+    // Se já houver um timer para esse elemento, limpe-o
+    if (timers[id]) {
+        clearInterval(timers[id]);
+    }
+
+    timers[id] = setInterval(function () {
+        current += increment;
+        obj.innerHTML = "+" + current; // Adiciona o sinal de "+" na frente dos valores
+        if (current == end) {
+            clearInterval(timers[id]);
+        }
     }, stepTime);
-  }
-  
-  // Chame a função para cada número com seus valores finais
-  animateValue("pessoas", 0, 27000, 5000, 24);
-  animateValue("eventos", 0, 470, 5000, 1);
-  animateValue("palestras", 0, 17, 5000, 1);
-  animateValue("cidades", 0, 14, 5000, 1);
-  
+}
+
+// Função para redefinir os valores dos números para zero e limpar os timers
+function resetValues() {
+    const elementsToReset = document.querySelectorAll('.number');
+    elementsToReset.forEach(element => {
+        element.innerHTML = "0";
+    });
+
+    // Limpa todos os timers
+    Object.values(timers).forEach(timer => {
+        clearInterval(timer);
+    });
+}
+
+// Chame a função para animar os números quando a seção de estatísticas estiver visível
+animateStatistics();
