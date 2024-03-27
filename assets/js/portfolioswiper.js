8f222cc5e4090e519fe8bdfb9a271dce17596ada
RickFerreira <richard.ferreira.ext@synchro.com.br>
2024-03-27 15:35:58 -0300

    Alterações de responsividade

--- assets/js/portfolioswiper.js
+++ assets/js/portfolioswiper.js
@@ -1,15 +1,70 @@
-var portfolioSwiper = new Swiper('.swiper-portfolio', {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    autoplay: {
-        delay: 2500, // Tempo de espera entre os slides em milissegundos
-    },
+var portfolioSwiper;
+
+function initSwiper() {
+    if (window.innerWidth < 426) {
+        if (!portfolioSwiper) {
+            portfolioSwiper = new Swiper('.swiper-portfolio', {
+                slidesPerView: 1,
+                spaceBetween: 30,
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true,
+                },
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                },
+                autoplay: {
+                    delay: 2500, // Tempo de espera entre os slides em milissegundos
+                },
+            });
+        }
+    } else if (window.innerWidth < 768) {
+        if (!portfolioSwiper) {
+            portfolioSwiper = new Swiper('.swiper-portfolio', {
+                slidesPerView: 2,
+                spaceBetween: 30,
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true,
+                },
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                },
+                autoplay: {
+                    delay: 2500, // Tempo de espera entre os slides em milissegundos
+                },
+            });
+        }
+    } else {
+        if (portfolioSwiper) {
+            portfolioSwiper.destroy();
+            portfolioSwiper = undefined;
+        }
+        portfolioSwiper = new Swiper('.swiper-portfolio', {
+            slidesPerView: 3,
+            spaceBetween: 30,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            autoplay: {
+                delay: 2500, // Tempo de espera entre os slides em milissegundos
+            },
+        });
+    }
+}
+
+// Inicialize o swiper quando a página for carregada e redimensionada
+window.addEventListener('load', function () {
+    initSwiper();
+});
+
+window.addEventListener('resize', function () {
+    initSwiper();
 });
