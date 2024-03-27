e475e69a70113e711206f9b31c9c6a916d3b877e
RickFerreira <richard.ferreira.ext@synchro.com.br>
2024-03-27 10:36:08 -0300

    Finalizando todas as modificações da primeira versão

---
+++ assets/js/portfolioswiper.js
@@ -0,0 +1,15 @@
+var portfolioSwiper = new Swiper('.swiper-portfolio', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 2500, // Tempo de espera entre os slides em milissegundos
+    },
+});
